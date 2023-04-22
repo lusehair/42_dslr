04dbd023ca2dc76242871cd5c5045304f2bef19f
E33aS42 <esommier@student.42.fr>
2023-04-22 14:01:18 +0200

    pair_plot done? might need some size adjustments

--- pair_plot.py
+++ pair_plot.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 		path = "datasets/dataset_train.csv"
 		data_train = pd.read_csv(path)
 
-		labels = data_train.select_dtypes(include=['int64', 'float64']).columns
+		labels = list(data_train.select_dtypes(include=['int64', 'float64']).columns)
 		# print(labels)
 		x_train = data_train[labels]
 		y_train = data_train[['Hogwarts House']].values
@@ -42,36 +42,15 @@ if __name__ == "__main__":
 		# 2. numerize y labels
 		Y_tr = num_houses(y_train)
 
+		# 3. create a distionary of features numpy arrays
 		X = {}
 		for feature in labels:
 			X[feature] = x_train[[feature]].values
 
-		# ax, fig = plt.subplots(len(labels), len(labels), figsize=(25, 25))
-		# i = 0
-		# j = 0
+		# 4. create pairplot using Seaborn
+		labels.remove('Index')
+		sns.pairplot(data=data_train[labels + ['Hogwarts House']], hue='Hogwarts House', diag_kind='kde', plot_kws={'s': 2}, height=1.5)
 
-		sns.pairplot(data=x_train, hue=Y_tr, diag_kind='kde')
-		# sns.histplot(X[feature][(Y_tr == 1)], bins=20, color='r', ax=fig[i][j], kde=True)
-		# for f1 in labels:
-		# 	# ax.tight_layout(pad=5.0)
-		# 	for f2 in labels:
-		# 		print(x_train[(Y_tr == 1)])
-		# 		sns.scatterplot(data=x_train[[feature]][(Y_tr == 1)], x=f1, y=f2, ax=fig[i][j])
-		# 		# fig[i][j].scatter(X[f1][(Y_tr == 1)], X[f2][(Y_tr == 1)], marker='o', c='r', label='Gryffindor')
-		# 		# fig[i][j].scatter(X[f1][(Y_tr == 2)], X[f2][(Y_tr == 2)], marker='o', c='y', label='Hufflepuff')
-		# 		# fig[i][j].scatter(X[f1][(Y_tr == 3)], X[f2][(Y_tr == 3)], marker='o', c='b', label='Ravenclaw')
-		# 		# fig[i][j].scatter(X[f1][(Y_tr == 4)], X[f2][(Y_tr == 4)], marker='o', c='g', label='Slytherin')
-
-		# 		if i == len(labels) - 1:
-		# 			fig[i][j].set_xlabel(f2)
-		# 		if j == 0:
-		# 			fig[i][j].set_ylabel(f1)
-		# 		# fig[i][j].legend()
-		# 		# fig[i][j].grid()
-		# 		j += 1
-		# 		if j == len(labels):
-		# 			i += 1
-		# 			j = 0
 
 		plt.show()
 
