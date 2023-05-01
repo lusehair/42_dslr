mini batch fixed. need to imporve computation time

bonus/logreg_train.py
@@ -195,7 +195,7 @@ if __name__ == "__main__":
 		models[i] = MyLR(np.ones((X_tr.shape[1] + 1, 1)), alpha=6e-3, max_iter=3000)
 		# x_step, loss_time = models[i].fit_(X_tr, y_[i])
 		# x_step, loss_time = models[i].fit_SGD(X_tr, y_[i])
-		x_step, loss_time = models[i].fit_minibatch(X_tr, y_[i], batch_size=1)
+		x_step, loss_time = models[i].fit_minibatch(X_tr, y_[i], batch_size=10)
 		# print(models[i].theta)
 		plt.plot(x_step, loss_time, label=houses[i-1], linewidth=2, c=colors[i-1])
 	time_end = time.time()

bonus/my_logistic_regression.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import log, exp
 import random as rd
+from copy import deepcopy
 
 class MyLogisticRegression():
 	"""
@@ -295,8 +296,9 @@ class MyLogisticRegression():
 			assert np.any(x) or np.any(y), "arguments cannot be empty numpy.ndarray"
 
 			# shuffle input data
-			x_shuffle = x[:]
-			y_shuffle = y[:]
+			
+			x_shuffle = deepcopy(x[:])
+			y_shuffle = deepcopy(y[:])
 			rdi = rd.randint(0, 100)
 			np.random.seed(rdi)
 			np.random.shuffle(x_shuffle, )
@@ -340,7 +342,7 @@ class MyLogisticRegression():
 			while step < self.max_iter:
 				# 1.select batch_size random observation datapoints
 				xi, yi = self.create_batch(x, y, batch_size)
-				# print(xi.shape, yi.shape)
+				# print(xi, yi)
 				# 1 shuffle dataset
 				# x_shuffle = x[:]
 				# y_shuffle = y[:]